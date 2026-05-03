b321480dc0be5938f9d1d35396f61595807d2f04
priya <priyachintapalli95@gmail.com>
2026-05-03 16:32:54 +0530

    Update sum_numbers.py

--- basics/loops/sum_numbers.py
+++ basics/loops/sum_numbers.py
@@ -1,8 +1,5 @@
-def sum_n(n):
-    total = 0
-    for i in range(1, n+1):
-        total += i
-    return total
-
-num = int(input("Enter number: "))
-print("Sum:", sum_n(num))
+n=int(input("enter the no:"))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+    print(sum)
