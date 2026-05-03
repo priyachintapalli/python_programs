dac3007d3e7d48d9183effe2281a8da6e78b6c35
priya <priyachintapalli95@gmail.com>
2026-05-03 15:58:00 +0530

    Simplify prime number check logic
    Refactor prime number check to simplify logic and remove function.

--- basics/loops/prime_number.py
+++ basics/loops/prime_number.py
@@ -1,14 +1,11 @@
-def is_prime(n):
-    if n <= 1:
-        return False
+n = int(input("Enter number: "))
+
+if n > 1:
     for i in range(2, n):
         if n % i == 0:
-            return False
-    return True
-
-num = int(input("Enter number: "))
-
-if is_prime(num):
-    print("Prime Number")
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
 else:
     print("Not Prime")
